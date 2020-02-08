682f9c9efc24a06b127ffa6b08edfd22082cd0df
ingJcbDev <ing.jcb.dev@gmail.com>
2020-02-08 11:01:14 -0500

    Se realiza hasta el video 3.3. Inicio del proyecto - AngularBases

--- 01-myapp/src/app/tab1/tab1.page.ts
+++ 01-myapp/src/app/tab1/tab1.page.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+
+  ngOnInit() {
+    /* console.log('ngOnInit tab1'); */
+
+    let numero = 10;
+    // tslint:disable-next-line: no-debugger
+    // nos sirve para realizar un paso a paso de la aplicacion
+    // debugger;
+    numero += 10;
+    numero *= 2;
+    numero -= 10;
+    console.log({numero});
+  }
 
   constructor() {}
 
