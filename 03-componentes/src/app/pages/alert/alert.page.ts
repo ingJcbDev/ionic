b941bf21301d88492a1189816cfa7b561eaacece
ingJcbDev <ing.jcb.dev@gmail.com>
2020-02-18 22:32:38 -0500

    5.7. ion-alert

--- 03-componentes/src/app/pages/alert/alert.page.ts
+++ 03-componentes/src/app/pages/alert/alert.page.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.page.html',
-  styleUrls: ['./alert.page.scss'],
+  styleUrls: ['./alert.page.scss']
 })
 export class AlertPage implements OnInit {
 
-  constructor() { }
+  titulo: string;
 
-  ngOnInit() {
+  constructor(public alertController: AlertController) {}
+
+  ngOnInit() {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: blah => {
+            console.log('Cancelar');
+          }
+        },
+        {
+          text: 'Ok',
+          handler: () => {
+            console.log('Boton Ok');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
+  async presentAlertPrompt() {
+    const alert = await this.alertController.create({
+      header: 'Nombre',
+      inputs: [
+        {
+          name: 'txtMonbre',
+          type: 'text',
+          placeholder: 'Digitar Nombre...'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: (data) => {
+            console.log('Confirm Ok', data);
+            this.titulo = data.txtMonbre;
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
