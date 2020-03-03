00efad40e7ce73e7211cfe0bca37b00d170e4d9b
ingJcbDev <ing.jcb.dev@gmail.com>
2020-03-02 21:11:08 -0500

    5.13. ion-datetime - Parte 2 - Personalización

--- 03-componentes/src/app/pages/data-time/data-time.page.ts
+++ 03-componentes/src/app/pages/data-time/data-time.page.ts
@@ -8,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class DataTimePage implements OnInit {
 
   fechaNaci: Date = new Date();
+  customPickerOptions;
+  customDate;
 
   constructor() { }
 
   ngOnInit() {
+
+    this.customPickerOptions = {
+      buttons: [{
+        text: 'Save',
+        handler: (event) => { 
+        console.log('Clicked Save!')
+        console.log(event)
+        }
+      }, {
+        text: 'Log',
+        handler: () => {
+          console.log('Clicked Log. Do not Dismiss.');
+          return false;
+        }
+      }]
+    };
+
   }
 
   CambioFecha( event ) {
