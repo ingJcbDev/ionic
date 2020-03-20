25710aabc339776320e0b6ea7e14eb02ab4edea2
ingJcbDev <ing.jcb.dev@gmail.com>
2020-03-20 08:54:19 -0500

    5.21. ion-item-sliding

--- 03-componentes/src/app/pages/list/list.page.ts
+++ 03-componentes/src/app/pages/list/list.page.ts
@@ -1,20 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Observable } from 'rxjs';
+import { IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.page.html',
-  styleUrls: ['./list.page.scss'],
+  styleUrls: ['./list.page.scss']
 })
 export class ListPage implements OnInit {
 
+  @ViewChild('lista', {static: false}) lista: IonList;
+
+  // @ViewChild( IonInfiniteScroll , {static: false} ) infitniteScroll: IonInfiniteScroll;
+
   usuarios: Observable<any>;
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService) {}
 
   ngOnInit() {
-  this.usuarios = this.dataService.getUsers();
+    this.usuarios = this.dataService.getUsers();
   }
 
+  favorite(user) {
+    console.log('favorite', user);
+    this.lista.closeSlidingItems();
+  }
+  share(user) {
+    console.log('share', user);
+    this.lista.closeSlidingItems();
+  }
+  borrar(user) {
+    console.log('borrar', user);
+    this.lista.closeSlidingItems();
+  }
+   
 }
