2451e1a7ca0cc1654343fda333d3bd37e597d255
ingJcbDev <ing.jcb.dev@gmail.com>
2020-05-11 18:37:13 -0500

    Se fin #5.37. ion-tabs

--- 03-componentes/src/app/pages/tabs/tabs-routing.module.ts
+++ 03-componentes/src/app/pages/tabs/tabs-routing.module.ts
@@ -5,8 +5,26 @@ import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
+    path: '', // nombre del tab
+    redirectTo: 'contact' // ruta del tab
+  },
+  {
     path: '',
-    component: TabsPage
+    component: TabsPage,
+    children: [
+    {
+      path: 'account', // nombre del tab
+      loadChildren: '../avatar/avatar.module#AvatarPageModule' // ruta del tab
+    },
+    {
+      path: 'contact', // nombre del tab
+      loadChildren: '../list/list.module#ListPageModule' // ruta del tab
+    },
+    {
+      path: 'settings', // nombre del tab
+      loadChildren: '../infinite-scroll/infinite-scroll.module#InfiniteScrollPageModule' // ruta del tab
+    }
+  ]
   }
 ];
 
