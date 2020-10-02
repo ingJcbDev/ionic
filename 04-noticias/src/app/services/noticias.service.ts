1d51e518933340cc2a95bab35c6fb3cb7f4c6e82
ingJcbDev <ing.jcb.dev@gmail.com>
2020-10-02 15:41:41 -0500

    7.6. Interfaces para el manejo de la información

--- 04-noticias/src/app/services/noticias.service.ts
+++ 04-noticias/src/app/services/noticias.service.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RespuestaToHeadLines } from '../pages/interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class NoticiasService {
   constructor(private http: HttpClient ) { }
 
   getTopHeadLines(){
-    return this.http.get(`http://newsapi.org/v2/top-headlines?country=co&apiKey=d268e175440744edbba4a0b3bffecfbe`);
+    return this.http.get<RespuestaToHeadLines>(`http://newsapi.org/v2/top-headlines?country=co&apiKey=d268e175440744edbba4a0b3bffecfbe`);
   }
 
 }
